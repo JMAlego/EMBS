923850b04a9c7ba6d1f72939d8eb63ef5138036a
Jacob Allen <jacob@polyomino.xyz>
2020-01-23 16:30:44 +0000

    Added libs and started UML

---
+++ EMBS_Q2/Diagrams/build_diagrams.bat
@@ -0,0 +1,2 @@
+rem https://plantuml.com/incubation
+@java -jar plantuml.jar Source.plantuml
--- EMBS_Q2/build.bat
+++ EMBS_Q2/build.bat
@@ -4,7 +4,7 @@ if not exist %MRC% (
   set "MRC=C:\Moterunner\moterunner\win32\bin\mrc.exe"
 )
 
-%MRC% --verbose --assembly=Source-4.0 embs\Source.java -r:logger-11.0
+%MRC% --verbose --assembly=Source-5.0 embs\Source.java -r:logger-11.0
 %MRC% --verbose --assembly=SinkA-1.0 embs\SinkA.java -r:logger-11.0
 %MRC% --verbose --assembly=SinkB-1.0 embs\SinkB.java -r:logger-11.0
 %MRC% --verbose --assembly=SinkC-1.0 embs\SinkC.java -r:logger-11.0
